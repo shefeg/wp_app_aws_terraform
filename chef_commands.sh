76adcbdfea25b6f57d9663386a399145f103dcdb
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2018-02-23 20:03:44 +0200

    Hack

--- chef_commands.sh
+++ chef_commands.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 git clone -b wp-app --single-branch https://github.com/shefeg/chef-repo.git; cd chef-repo || cd chef-repo; git pull origin wp-app
-sleep 5
+sleep 5 # hack...
 NODE="$(sudo knife node list -z 2>&1 | tail -1)"
 sudo knife node run_list add $NODE wp-app::default -z
 sudo chef-client -z
