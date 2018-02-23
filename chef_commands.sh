217b47462a1874376e29b60df444389551affe08
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2018-02-23 20:12:05 +0200

    Hacks

--- chef_commands.sh
+++ chef_commands.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-git clone -b wp-app --single-branch https://github.com/shefeg/chef-repo.git; cd chef-repo || cd chef-repo; git pull origin wp-app
-sleep 5 # hack...
-NODE="$(sudo knife node list -z 2>&1 | tail -1)"
-sudo knife node run_list add $NODE wp-app::default -z
+git clone -b wp-app --single-branch https://github.com/shefeg/chef-repo.git; cd chef-repo || \
+cd chef-repo; git pull origin wp-app
+while ! NODE="$(sudo knife node list -z 2>&1 | tail -1)" && sudo knife node run_list add $NODE wp-app::default -z; do 
+    echo "Add cookbook to run list failed. Retrying..." >&2
+done
 sudo chef-client -z
