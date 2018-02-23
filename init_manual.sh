f0563bb49e23afdf31927ff52dac091865cdbe88
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2018-02-23 19:54:01 +0200

    Fixes

--- init_manual.sh
+++ init_manual.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # DESCRIPTION:
-# Script to run manually to create AWS: user, access keys and policies for Terraform
+# Script to run manually to create AWS user and add policies for Terraform
 
 USER="terraform"
-REGION="us-east-1"
 
 aws iam create-user --user-name $USER
 aws iam create-access-key --user-name $USER
