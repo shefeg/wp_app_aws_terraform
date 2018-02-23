3841763b46e2cbeff4a92b7f721eecefd7853274
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2018-02-23 21:15:24 +0200

    Fix

--- chef_commands.sh
+++ chef_commands.sh
@@ -2,12 +2,7 @@
 
 git clone -b wp-app --single-branch https://github.com/shefeg/chef-repo.git; cd chef-repo || \
 cd chef-repo; git pull origin wp-app
-while ! sudo chef-client cookbooks/wp-app/recipes/default.rb -z; do 
-    echo "Wait for chef-client. Retrying..." && sleep 5
+while ! (NODE="$(sudo knife node list -z 2>&1 | tail -1)" && sudo knife node run_list add $NODE wp-app::default -z); do 
+    echo "Add cookbook to run list failed. Retrying..." && sleep 10
 done
 
-# sudo chef-client cookbooks/wp-app/recipes/default.rb -z
-
-# while ! (NODE="$(sudo knife node list -z 2>&1 | tail -1)" && sudo knife node run_list add $NODE wp-app::default -z); do 
-#     echo "Add cookbook to run list failed. Retrying..." && sleep 5
-# done
