d93d90105164b33b7daee8e4219833ded71173ec
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2018-02-23 12:39:18 +0200

    Testing pipeline

--- chef_commands.sh
+++ chef_commands.sh
@@ -2,8 +2,7 @@
 
 git clone -b wp-app --single-branch https://github.com/shefeg/chef-repo.git
 cd chef-repo
-
 NODE="$(sudo knife node list -z 2>&1 | tail -1)"
-
 sudo knife node run_list add $NODE wp-app::default -z
 chef-client -z
+[ $? -eq 0 ] || exit 1
