26d37d0e99f2e2e338762ba2ae9ad61ae655a61f
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2018-02-24 09:56:40 +0200

    Fix

--- chef_commands.sh
+++ chef_commands.sh
@@ -2,7 +2,7 @@
 
 git clone -b wp-app --single-branch https://github.com/shefeg/chef-repo.git; cd chef-repo || \
 cd chef-repo; git pull origin wp-app
-sudo chef-client -z
+[ -f /var/www/html/wp-config.php ] && sudo chef-client -z
 NODE="$(sudo knife node list -z 2>&1 | tail -1)"
 sudo knife node run_list add $NODE wp-app::default -z
 sudo chef-client -z
