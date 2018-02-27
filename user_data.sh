3fe8dc031251a0becade6cf1be4dc457d3e0f685
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2018-02-27 16:32:40 +0200

    Fix

--- user_data.sh
+++ user_data.sh
@@ -2,7 +2,7 @@
 
 chefdk_install_ubuntu_16 ()
 {
-    if [[ ! $(dpkg -l | grep chefdk) ]]; then
+    if [ ! $(dpkg -l | grep chefdk) ]; then
         CHEF_PACKAGE_URL="https://packages.chef.io/files/stable/chefdk/2.4.17/ubuntu/16.04/chefdk_2.4.17-1_amd64.deb"
         wget -nc $CHEF_PACKAGE_URL
         dpkg -i $(echo "$CHEF_PACKAGE_URL" | sed 's/^.*\(chef\)/\1/')
@@ -16,7 +16,7 @@ chefdk_install_ubuntu_16 ()
 
 chefdk_install_centos_7 ()
 {
-    if [[ ! $(sudo rpm -qa | grep chefdk) ]]; then
+    if [ ! $(sudo rpm -qa | grep chefdk) ]; then
         CHEF_PACKAGE_URL="https://packages.chef.io/files/stable/chefdk/2.4.17/el/7/chefdk-2.4.17-1.el7.x86_64.rpm"
         sudo rpm -Uvh $CHEF_PACKAGE_URL
         [ ! -f ~/.bash_profile ] && sudo touch ~/.bash_profile
