c7c606e1c1362cd855d4dfed217148da796403bf
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2018-02-28 11:16:24 +0200

    Fixes

--- .circleci/circleci_init.sh
+++ .circleci/circleci_init.sh
@@ -4,10 +4,10 @@
 # Script to create S3 bucket and Dynamo DB table for terraform remote state storage
 # Should be run at the start of CircleCI pipeline
 
-# REGION="us-east-1"
-
+REGION=${REGION:-"us-east-1"}
+BUCKET=${BUCKET:-"terraform-remote-state-storage-s3-oihn"}
 set -e
-BUCKET="terraform-remote-state-storage-s3-oihn"
+
 aws s3api create-bucket --bucket $BUCKET --region $REGION && \
 aws s3api put-bucket-versioning --bucket $BUCKET --versioning-configuration Status=Enabled
 [[ "$(aws dynamodb create-table \
@@ -15,4 +15,4 @@ aws s3api put-bucket-versioning --bucket $BUCKET --versioning-configuration Stat
          --table-name terraform_locks \
          --attribute-definitions AttributeName=LockID,AttributeType=S \
          --key-schema AttributeName=LockID,KeyType=HASH \
-         --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 2>&1)" = *"ResourceInUseException"* ]] || true
+         --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 2>&1)" = *"ResourceInUseException"* ]] || false
