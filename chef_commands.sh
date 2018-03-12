dea1589932d301c0adf29fc2dc3aae379f74e322
Alexey Ignatenko <a.ignatenko1988@gmail.com>
2018-03-12 21:11:18 +0200

    Fix

--- chef_commands.sh
+++ chef_commands.sh
@@ -2,8 +2,8 @@
 
 [[ "$(. /etc/os-release; echo $NAME)" = "Ubuntu" ]] || sudo yum install -y install git
 cd ~
-git clone -b wp-app --single-branch https://github.com/shefeg/chef-repo.git && cd ~/chef-repo || \
-cd ~/chef-repo && git pull origin wp-app
+git clone -b master --single-branch https://github.com/shefeg/chef-repo.git && cd ~/chef-repo || \
+cd ~/chef-repo && git pull origin master
 [ -f /var/www/html/wp-config.php ] || sudo chef-client -z
 NODE="$(sudo knife node list -z 2>&1 | tail -1)"
 sudo knife node run_list add $NODE wp-app::default -z
